9e72d32247f3c305f24d9dc79e46123da6156f91
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2025-10-18 18:02:46 +0100

    add button css classes

--- css/css_examples.c
+++ css/css_examples.c
@@ -1,13 +1,15 @@
-#include "glib-object.h"
 #include <gtk/gtk.h>
 
 static void on_activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
     GtkWidget *box;
     GtkWidget *label;
+    GtkWidget *grid;
+    GtkWidget *button;
 
     window = gtk_application_window_new(app);
     box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 12);
+    gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
     gtk_window_set_child(GTK_WINDOW(window), box);
 
     //////////////////////////////////////////
@@ -35,6 +37,31 @@ static void on_activate(GtkApplication *app, gpointer user_data) {
     label = gtk_label_new("Heading");
     gtk_widget_add_css_class(label, "heading");
     gtk_box_append(GTK_BOX(box), label);
+
+    label = gtk_label_new("Body default");
+    gtk_box_append(GTK_BOX(box), label);
+
+    //////////////////////////////////////////
+    // Button CSS classes
+    grid = gtk_grid_new();
+    gtk_grid_set_column_spacing(GTK_GRID(grid), 6);
+    gtk_box_append(GTK_BOX(box), grid);
+
+    button = gtk_button_new_with_label("Default");
+    gtk_grid_attach(GTK_GRID(grid), button, 0, 0, 1, 1);
+
+    button = gtk_button_new_with_label("Destructive");
+    gtk_widget_add_css_class(button, "destructive-action");
+    gtk_grid_attach(GTK_GRID(grid), button, 1, 0, 1, 1);
+
+    button = gtk_button_new_with_label("Suggested");
+    gtk_widget_add_css_class(button, "suggested-action");
+    gtk_grid_attach(GTK_GRID(grid), button, 2, 0, 1, 1);
+
+    button = gtk_button_new_from_icon_name("list-add");
+    gtk_widget_add_css_class(button, "circular");
+    gtk_grid_attach(GTK_GRID(grid), button, 3, 0, 1, 1);
+
 
     gtk_window_present(GTK_WINDOW(window));
 }
